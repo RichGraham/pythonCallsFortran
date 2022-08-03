4150d762cf190f062305f5b22e7d98ccc2f24c4d
Richard S. Graham <RichGraham@users.noreply.github.com>
2022-08-03 11:41:47 +0100

    Array now called as a function

--- wArray/callFortran.py
+++ wArray/callFortran.py
@@ -8,10 +8,11 @@ f = fortlib.double_array
 # Specify arguments type as a pointer to double and an integer
 f.argtypes=[ct.POINTER(ct.c_double), ct.c_int]
 
-# Create a double array, pass it to Fortran as a pointer
-x = np.ones((3,3), order="F")
-x_ptr = x.ctypes.data_as(ct.POINTER(ct.c_double))
+def callFunction():
+    # Create a double array, pass it to Fortran as a pointer
+    x = np.ones((3,3), order="F")
+    x_ptr = x.ctypes.data_as(ct.POINTER(ct.c_double))
 
-# Call function
-rint = f(x_ptr, ct.c_int(3))
-print(x)
+    # Call function
+    rint = f(x_ptr, ct.c_int(3))
+    return x
